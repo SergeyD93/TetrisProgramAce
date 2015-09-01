296c58c388ec893a150b4e46650646b4e931b6b4
SergeyD93 <Sergeydurnev93@gmail.com>
2015-09-01 20:45:32 +0300

    MainMenu and shader test

---
+++ TetrisProgramAce/Assets/ProjectAssets/Scripts/ShaderUsing.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShaderUsing : MonoBehaviour {
+
+    float alpha;
+    float alphaDelta = 0.05f;
+	void Start () 
+    {
+        alpha = gameObject.GetComponent<Renderer>().material.color.a;
+        StartCoroutine(TestCoroutine());
+	}
+
+    IEnumerator TestCoroutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (alpha > 0)
+        {
+            alpha -= alphaDelta;
+
+            Color color = new Color(gameObject.GetComponent<Renderer>().material.color.r, gameObject.GetComponent<Renderer>().material.color.g, gameObject.GetComponent<Renderer>().material.color.b, alpha);
+            Renderer rend = gameObject.GetComponent<Renderer>();
+            rend.material.SetColor("_Color", color);
+            StartCoroutine(TestCoroutine());
+        }
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TetrisProgramAce/Assets/Shaders/Custom/TransparentCustomShader.shader
@@ -0,0 +1,19 @@
+﻿Shader "Custom/TransparentCustomShader" {
+	Properties {
+    _Color ("Main Color", Color) = (1,1,1,1)
+
+}
+SubShader
+{
+
+	Tags {"Queue" = "Transparent"}
+	Lighting Off
+         ZWrite Off
+         Cull Back
+	Blend SrcAlpha OneMinusSrcAlpha
+	Color[_Color]
+	 Pass {
+         }
+}
+	FallBack "Diffuse"
+}
